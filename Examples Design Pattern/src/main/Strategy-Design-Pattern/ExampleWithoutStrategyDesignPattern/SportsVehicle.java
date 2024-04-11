243a7d57f4e1edfdb7a064ce4b6daaf7f40e3468
muskanjaiswal10 <meetpatel96301@gmail.com>
2024-04-11 16:11:26 +0530

    Classes Created

---
+++ Examples Design Pattern/src/main/Strategy-Design-Pattern/ExampleWithoutStrategyDesignPattern/SportsVehicle.java
@@ -0,0 +1,7 @@
+package ExampleWithoutStrategyPattern;
+
+public class SportsVehicle extends  Vehicle(){
+    public void drive(){
+        System.out.println("sports Drive Capability");
+    }
+}
---
+++ Examples Design Pattern/src/main/Strategy-Design-Pattern/ExampleWithoutStrategyDesignPattern/Vehicle.java
@@ -0,0 +1,7 @@
+package ExampleWithoutStrategyPattern;
+
+public class Vehicle(){
+    public void drive(){
+        System.out.println("normal Drive Capability");
+    }
+}
